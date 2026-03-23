d642ed89a01cb8c15f1d43c4fd3c92bfa823f8e5
iceicelychee <https://github.com/iceicelychee/paperManageproject.git>
2026-03-23 23:28:50 +0800

    添加环境变量和忽略规则

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
+import process from 'process'
+
 const mysql = require('mysql2');
+require('dotenv').config()
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'mysql123',
-    database: 'blog_system'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 db.connect(err => {
