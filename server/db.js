e95f85da6a763aadbb9e365eaea352eea1042101
iceicelychee <https://github.com/iceicelychee/paperManageproject.git>
2026-03-24 18:52:00 +0800

    dist

--- server/db.js
+++ server/db.js
@@ -3,12 +3,21 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const db = mysql.createConnection({
+/*const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-})
+})*/
+// 优先使用 DATABASE_URL，否则使用独立的环境变量
+const db = process.env.DATABASE_URL 
+    ? mysql.createConnection(process.env.DATABASE_URL)
+    : mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+ })
 
 db.connect(err => {
     if(err) {
